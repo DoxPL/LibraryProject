3bdfa54b5994a2dee0490194a3457ea616662288
DoxPL <dox100@gmail.com>
2019-01-04 15:51:15 +0100

    Changed branch structure

---
+++ Biblioteka/Biblioteka/Orders.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Biblioteka
+{
+    public partial class Orders : Form
+    {
+        DataClasses1DataContext dbDataContext = new DataClasses1DataContext();
+        public Orders()
+        {
+            InitializeComponent();
+        }
+
+        private void Orders_Load(object sender, EventArgs e)
+        {
+            loadOrders();
+        }
+
+        private void loadOrders()
+        {
+            foreach (BookRental bookRental in dbDataContext.BookRentals.OrderBy(x => x.RentDate))
+            {
+                Users user = dbDataContext.Users.Where(x => x.ID == bookRental.ReaderID).First();
+                BookCopy bookCopy = dbDataContext.BookCopies.Where(x => x.ID == bookRental.CopyID).First();
+                Books book = dbDataContext.Books.Where(x => x.ID == bookCopy.BookID).First(); //name
+                this.lOrders.Items.Add(
+                        (user.Name + " " + user.Surname) + "\t" +
+                        bookRental.RentDate + "\t" +
+                        bookRental.ReturnDate + "\t" +
+                        book.Title
+                    );
+            }
+        }
+
+        private void lOrders_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- dbo.Copy.data.sql
+++
@@ -1,3 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[Copy] ON
-INSERT INTO [dbo].[Copy] ([Id], [bookId]) VALUES (1, 2)
-SET IDENTITY_INSERT [dbo].[Copy] OFF
