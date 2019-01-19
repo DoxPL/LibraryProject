4067e810670c85178345a462490450ea0ea61f48
Rick <delpatmg@gmail.com>
2019-01-19 19:04:19 +0100

    picturebox

---
+++ Biblioteka/Biblioteka/EmailAccountModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka
+{
+    class EmailAccountModel
+    {
+        private string host;
+        private string username;
+        private string password;
+        private string address;
+        
+        public EmailAccountModel(string host, string address, string login, string pass)
+        {
+            this.host = host;
+            this.username = login;
+            this.password = pass;
+            this.address = address;
+        }
+
+        public string getHost()
+        {
+            return this.host;
+        }
+
+        public string getLogin()
+        {
+            return this.username;
+        }
+
+        public string getPasswd()
+        {
+            return this.password;
+        }
+
+        public string getAddress()
+        {
+            return this.address;
+        }
+
+    }
+}
